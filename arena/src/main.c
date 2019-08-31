20a0f65862f3bc9d4e9ba593a061836c125dd261
Cizeur <cgiron@e1r4p8.42.fr>
2019-08-31 16:50:16 +0200

    main with init and file loading

--- arena/src/main.c
+++ arena/src/main.c
@@ -6,11 +6,21 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 09:59:05 by cgiron            #+#    #+#             */
-/*   Updated: 2019/08/31 10:01:25 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/08/31 16:09:27 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				main(char **argv, int argc)
+#include "libft/libft.h"
+#include "arena.h"
+
+int				main(int argc, char **argv)
 {
+	t_master	*mstr;
+
+	if (!(--argc))
+		ft_putstr(USAGE_STR);
+	mstr = 0;
+	init(&mstr);
+	file_loading(mstr, argc, argv);
 	return (0);
 }
