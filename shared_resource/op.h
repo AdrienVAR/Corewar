2bb45836afecf782e4c4d12e0d3ad0481c662d2d
Cizeur <cgiron@e1r4p8.42.fr>
2019-08-31 16:51:05 +0200

    normed the op.h file

--- shared_resource/op.h
+++ shared_resource/op.h
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2019/08/31 10:45:54 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/08/31 11:04:55 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,37 +21,60 @@
 # define REG_SIZE				4
 # define DIR_SIZE				REG_SIZE
 
-
 # define REG_CODE				1
 # define DIR_CODE				2
 # define IND_CODE				3
 
+/*
+**		****************
+** 	**	ARENA PARAMETERS **
+**		****************
+*/
 
-# define MAX_ARGS_NUMBER			4
-# define MAX_PLAYERS				4
+# define MAX_ARGS_NUMBER		4
+# define MAX_PLAYERS			4
 # define MEM_SIZE				(4*1024)
-# define IDX_MOD					(MEM_SIZE / 8)
+# define IDX_MOD				(MEM_SIZE / 8)
 # define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+
+/*
+**		**************************************
+** 	**	ASSEMBLY MARKERS and LABEL LIMITATIONS **
+**		**************************************
+*/
 
 # define COMMENT_CHAR			'#'
 # define LABEL_CHAR				':'
-# define DIRECT_CHAR				'%'
+# define DIRECT_CHAR			'%'
 # define SEPARATOR_CHAR			','
+# define LABEL_CHARS			"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-# define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+/*
+**		*****************
+** 	**	SIGNATURE STRINGS **
+**		*****************
+*/
 
-# define NAME_CMD_STRING			".name"
+# define NAME_CMD_STRING		".name"
 # define COMMENT_CMD_STRING		".comment"
 
 # define REG_NUMBER				16
 
+/*
+**		***************
+** 	**	GAME PARAMETERS **
+**		***************
+*/
+
 # define CYCLE_TO_DIE			1536
-# define CYCLE_DELTA				50
+# define CYCLE_DELTA			50
 # define NBR_LIVE				21
 # define MAX_CHECKS				10
 
 /*
-**
+**		***************
+** 	**	PARAMETER TYPE  **
+**		***************
 */
 
 typedef char	t_arg_type;
@@ -62,19 +85,21 @@ typedef char	t_arg_type;
 # define T_LAB					8
 
 /*
-**
+**		***********************************
+** 	**	SIZE LIMITATIONS AND EXEC SIGNATURE  **
+**		***********************************
 */
 
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
 # define COREWAR_EXEC_MAGIC		0xea83f3
 
-typedef struct		header_s
+typedef struct		s_header
 {
-  unsigned int		magic;
-  char				prog_name[PROG_NAME_LENGTH + 1];
-  unsigned int		prog_size;
-  char				comment[COMMENT_LENGTH + 1];
-}					header_t;
+	unsigned int		magic;
+	char				prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int		prog_size;
+	char				comment[COMMENT_LENGTH + 1];
+}					t_header;
 
 #endif
