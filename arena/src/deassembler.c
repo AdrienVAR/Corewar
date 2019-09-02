6d5d2cdaa2a21ef6059d39299709e6e184341596
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-02 12:22:18 +0200

    deassembler kinda working

--- arena/src/deassembler.c
+++ arena/src/deassembler.c
@@ -6,30 +6,32 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 09:02:21 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/02 10:28:07 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/02 12:20:15 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arena.h"
-#include "libft/libft.h"
 
-
-static void		ft_deassembler_name(t_master *mstr, t_player *player)
-{
-	mstr = 0;
-	player = 0;
-}
 
 void			deassembler(t_master *mstr)
 {
-	int i;
-	t_player *player;
+	int			i;
+	t_player	*player;
 
 	i = -1;
 	while (++i < mstr->nb_of_players)
 	{
 		player = mstr->players[i];
 		player->magic = binary_read_integer(player->fd, mstr);
-		ft_deassembler_name(mstr, mstr->players[i]);
+		binary_read_string(
+			player->fd, &(player->name[0]), PROG_NAME_LENGTH, mstr);
+		binary_read_null(player->fd, mstr);
+		player->code_size = binary_read_integer(player->fd, mstr);
+		binary_read_string(
+			player->fd, &(player->comment[0]), COMMENT_LENGTH, mstr);
+		binary_read_null(player->fd, mstr);
+		if (player->code_size < CHAMP_MAX_SIZE && player->code_size > 0)
+		binary_read_string(
+			player->fd, &(player->exec[0]), player->code_size - 1, mstr);
 	}
 }
