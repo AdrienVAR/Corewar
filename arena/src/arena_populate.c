dc2ff5ca93e647ea1d09dfc4dbf01bed1e778aec
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-02 14:29:03 +0200

    putplayer in arena

---
+++ arena/src/arena_populate.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arena_populate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/02 13:28:23 by cgiron            #+#    #+#             */
+/*   Updated: 2019/09/02 14:27:00 by cgiron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "arena.h"
+#include "libft/libft.h"
+
+void		arena_populate(t_master *mstr)
+{
+	char			*arena;
+	t_player		*player;
+	int				step;
+	int				i;
+
+	i = -1;
+	arena = &(mstr->arena[0]);
+	step = MEM_SIZE / mstr->nb_of_players;
+	while (++i < mstr->nb_of_players)
+	{
+		player = mstr->players[i];
+		ft_memcpy(arena + i * step, player->exec, player->code_size);
+	}
+}
