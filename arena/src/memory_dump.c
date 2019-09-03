b53a1b26cdfd04d2a5151ca33a839a6a9cf9df93
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-03 17:36:06 +0200

    norm

--- arena/src/memory_dump.c
+++ arena/src/memory_dump.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 15:26:23 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/02 15:51:51 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/03 11:54:35 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 void			memory_dump(t_master *mstr)
 {
-	char *arena;
+	char		*arena;
 	static char hexa[17] = "0123456789abcdef";
-	int i;
+	int			i;
 
 	arena = mstr->arena;
 	i = -1;
