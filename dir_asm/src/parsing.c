3b19d9f22300edd72504cde792bf515b4bbeab77
Adrien VARDON <advardon@e1r4p9.42.fr>
2019-09-04 16:01:04 +0200

    added chained list asm_line

--- dir_asm/src/parsing.c
+++ dir_asm/src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: advardon <advardon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 14:55:51 by gdrai             #+#    #+#             */
-/*   Updated: 2019/09/04 09:12:39 by advardon         ###   ########.fr       */
+/*   Updated: 2019/09/04 15:37:49 by advardon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,6 @@ char	*get_file_content(char *file)
 		clean_exit(NULL, "Opening file has failed\n");
 	if (!(buffer = ft_strdup("\0")))
 		clean_exit(NULL, "Memory allocation has failed\n");
-	if (get_next_line(fd, &line) < 0)
-	{
-		checker_name(line);
-		if (!(buffer = ft_strjoin_free(buffer, line)))
-			clean_exit(buffer, "Memory allocation has failed\n");
-		ft_strdel(&line);
-	}
 	while (get_next_line(fd, &line) == 1)
 	{
 		if (!(buffer = ft_strjoin_free(buffer, line)))
@@ -46,8 +39,47 @@ char	*get_file_content(char *file)
 
 void	parsing(char *file)
 {
-	char 	*buffer;
+	t_asm_line	*head;
+	t_asm_line	*instruction;
+	char		*instruct[4];
 
-	buffer = get_file_content(file);
-	ft_putstr(buffer);
+	instruct[0] = "arriere:";
+	instruct[1] = "ld";
+	instruct[2] = "%-5";
+	instruct[3] = "r5";
+	instruct[4][0] = '\0';
+
+	head = NULL;
+	(void) file;
+	//get_file_content(file);
+	instruction = ft_lstadd_end(head);
+	instruction->line_splitted = instruct;
+	//instruction->label = check_label();
+	ft_putstr(instruction->line_splitted[2]);
 }
+
+/*
+** Create a new struct and add it at end of the list.
+*/
+
+t_asm_line	*ft_lstadd_end(t_asm_line *lst)
+{
+	t_asm_line	*lstnew;
+	t_asm_line	*lnext;
+
+	if (!(lstnew = (t_asm_line *)ft_memalloc(sizeof(t_asm_line))))
+		return (NULL);
+	if (!lst)
+		return (lstnew);
+	lstnew->next = NULL;
+	lnext = lst;
+	while (lnext->next)
+		lnext = lnext->next;
+	lnext->next = lstnew;
+	return (lst);
+}
+
+/* type = 0
+type = T_IND
+type = type << 2
+type &= T_REG */
