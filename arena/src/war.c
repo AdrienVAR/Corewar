0d57b75588c9b291d4c9308e774b7fdb5f0465ef
Cizeur <cgiron@e1r5p9.42.fr>
2019-09-04 19:08:19 +0200

    added support read of parameters and starting the execution

--- arena/src/war.c
+++ arena/src/war.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 14:07:46 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/03 16:13:48 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/04 19:05:22 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,99 @@
 
 void			run_command(t_process *cur_process, char *arena)
 {
+	static void		(*command_functions[AVAILABLE_OPERATIONS])()=
+	{};
+
+	command_functions[0] = 0;
 	cur_process = 0;
 	arena = 0;
 	ft_putstr("RUUUUUN COMMAND RUUUUUUUN !!!\n");
 }
 
-void			set_wait_time(t_process *cur_process, char pc)
+void			command_get_types_size(t_process *cur_process)
+{
+	int i;
+	int type;
+	int type_size;
+
+	i = -1;
+	while (++i < MAX_ARGS_NUMBER)
+	{
+		type_size = 0;
+		type = 0;
+		type = cur_process->vm.command.types[i];
+		type_size = type == DIR_CODE ? DIR_SIZE : type_size;
+		type_size = type == REG_CODE ? REG_SIZE : type_size;
+		type_size = type == IND_CODE ? IND_SIZE : type_size;
+		cur_process->vm.command.types_size[i] = type_size;
+	}
+}
+
+void			command_get_types(t_process *cur_process, uchar type_code)
+{
+	int i;
+	char type;
+
+	i = MAX_ARGS_NUMBER;
+	while (--i >= 0)
+	{
+		type = type_code & 3;
+		type_code >>= 2;
+		cur_process->vm.command.types[i] = (int)type;
+	}
+}
+
+void			command_get_info(t_process *cur_process, uchar op_code)
 {
 	t_op operation;
+	t_vm_pcs_track vm;
 
-	operation = operation_get_info(pc);
-	cur_process->vm.wait = operation.duration;
+	operation = operation_get_info(op_code);
+	vm = cur_process->vm;
+	vm.command.op = operation;
+	vm.wait = operation.duration;
+	cur_process->vm = vm;
 }
+
+
+void			command_get_param(t_process *cur_process, char *arena)
+{
+	t_command		command;
+	int				pc;
+	int				i;
+	int				j;
+
+	i = -1;
+	pc = (cur_process->pc + 2) % MEM_SIZE;
+	command = cur_process->vm.command;
+	while (++i < command.op.nb_params)
+	{
+		j = -1;
+		while (++j < command.types_size[i])
+			(command.param)[j][i] = arena_val(arena, pc++);
+	}
+	cur_process->vm.command = command;
+}
+
 
 void			run_cycle(t_master *mstr)
 {
-	t_process *cur_process;
+	t_process	*cur_process;
+	char		*arena;
 
+	arena = mstr->arena;
 	cur_process = mstr->process;
 	while (cur_process)
 	{
 		if (cur_process->vm.wait == 1)
-			run_command(cur_process, mstr->arena);
+			run_command(cur_process, arena);
 		else if (!cur_process->vm.wait)
-			set_wait_time(cur_process, mstr->arena[cur_process->pc]);
+		{
+			command_get_info(cur_process, arena_val(arena, cur_process->pc));
+			command_get_types(cur_process, arena_val(arena, cur_process->pc + 1));
+			command_get_types_size(cur_process);
+			command_get_param(cur_process, arena);
+		}
 		cur_process->vm.wait--;
 		cur_process = cur_process->next;
 	}
