5a07f8122e4bf1e548d37590d0965b7719d39823
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-05 18:42:44 +0200

    ld direct working

--- arena/src/ex_command_ld.c
+++ arena/src/ex_command_ld.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   command_ld.c                                       :+:      :+:    :+:   */
+/*   ex_command_ld.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 18:47:19 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/04 19:04:16 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/05 15:59:54 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arena.h"
 
-void			command_ld(t_process process, char *arena)
+void			ex_command_ld(t_process *process, char *arena)
 {
 	t_command command;
-	t_int_reg_cast reg;
+
 	int i;
+	int reg;
+	int pc;
 
-	command = process.vm.command;
-	i = 0;
-	while (++i < REG_SIZE && i < )
-
-
-
-
+	command = process->vm.command;
+	pc = command.op.type_needed ? process->pc + 2 : process->pc + 1;
+	if (command_valid_types(command) == NO
+		|| (reg = command_valid_register(command, 1)) == NO)
+		return ;
+	i = -1;
+	while (++i < DIR_SIZE)
+		process->registry[reg][i] = arena_val(arena, pc + i);
+	printf("RUUUUUN COMMAND RUUUUUUUN !!!\n");
 }
