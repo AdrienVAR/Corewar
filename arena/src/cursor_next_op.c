d9fad28953ea9aab10bb303aa75166be2e6f5554
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-06 16:48:09 +0200

    manage untyped jump to check not sure

--- arena/src/cursor_next_op.c
+++ arena/src/cursor_next_op.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 15:41:18 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/05 18:28:34 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/06 16:48:05 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 void		cursor_next_op(t_process *process)
 {
-	int i;
-	int pc;
-	t_command command;
+	int 		i;
+	int			pc;
+	t_command	command;
 
 	command = process->vm.command;
 	pc = process->pc;
-	pc += command.op.type_needed ? 2 : 1;
-	i = -1;
-	while (++i < command.op.nb_params)
-		pc += command.types[i].size;
+	if (!command.op.type_needed)
+		pc += 1 + DIR_SIZE;
+	else
+	{
+		i = -1;
+		while (++i < command.op.nb_params)
+			pc += command.types[i].size;
+	}
 	process->pc = pc % MEM_SIZE;
 }
