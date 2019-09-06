6d287e5737e940d9d6bd3d685c24617e13bcf1b3
Gabriel DRAI <gdrai@e1r6p9.42.fr>
2019-09-06 15:20:18 +0200

    update split_line.c

--- dir_asm/src/split_line.c
+++ dir_asm/src/split_line.c
@@ -3,190 +3,180 @@
 /*                                                        :::      ::::::::   */
 /*   split_line.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advardon <advardon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gdrai <gdrai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 13:17:20 by gdrai             #+#    #+#             */
-/*   Updated: 2019/09/04 17:47:50 by advardon         ###   ########.fr       */
+/*   Updated: 2019/09/06 15:19:24 by gdrai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-
-/*
-** split_line 
-** 2 parameters : the line to analyse
-**                the option (0 if header, 1 if operation)
-**
-** return NULL : option not 0 or 1
-**               number of quotemark different from 0 or 2 in option 0
-**				 1 or 0 strings to return
-**
-** return a char** if evrything is OK
-*/
-
-int     count_tab(char *line)
+char	**op_case(char *line, int count)
 {
-    int i;
-    int j;
-    int count;
-    int k;
-
-    i = 0;
-    j = 0;
-    k = 1;
-    count = 0;
-    while (line[i] != '\0' && line[i] != COMMENT_CHAR)
-    {
-        if (line[i] == '"')
+	char **tab;
+	size_t	i;
+	size_t	j;
+	int		k;
+	int		first_word;
+	
+	if (!(tab = (char **)malloc(sizeof(char *) * (count + 1))))
+        clean_exit(NULL, "Memory allocation failed\n");
+	tab[count] = NULL;
+	i = 0;
+	k = 0;
+	first_word = 1;
+	while (k < count)
+	{
+		if (line[i] != ' ' && line[i] != '\t' && line[i] != SEPARATOR_CHAR)
 		{
-			k = 1;
-			j++;
-			if (j == 1)
-				count++;
+			j = i;
+			while (line[j] != ' ' && line[j] != '\t' && line[j] != SEPARATOR_CHAR && line[j] != '\0' && line[j] != COMMENT_CHAR)
+			{
+				j++;
+				if (line[j] == LABEL_CHAR && first_word)
+				{
+					j++;
+					break ;
+				}
+			}
+			if (!(tab[k] = ft_memalloc(j - i + 1)))
+        		clean_exit(NULL, "Memory allocation failed\n");
+			tab[k] = ft_memcpy(tab[k], line + i, j - i);
+			k++;
+			first_word = 0;
+			i = j;
 		}
-        else if ((line[i] == ' ' || line[i] == '\t') && j != 1)
-            k = 1;
-        else if (line[i] != ' ' && line[i] != '\t' && j != 1)
-        {
-            if (k == 1)
-            {
-                k = 0;
-                count++;
-            }
-        }
-        i++;
-    }
-    return (count);
-}
-
-int     check_quotemark(char *line)
-{
-    int count;
-    int i;
-
-    count = 0;
-    i = 0;
-    while (line[i] != '\0' && line[i] != COMMENT_CHAR)
-    {
-        if (line[i] == '"')
-            count++;
-        i++;
-    }
-    return (count);
-}
-
-char    *prepare_op_line(char *line)
-{
-    int	i;
-
-    i = 0;
-    while (line[i] != '\0' && line[i] != COMMENT_CHAR)
-    {
-        if (line[i] == SEPARATOR_CHAR)
-            line[i] = ' ';
-        i++;
-    }
-	i = 0;
-	while (line[i] != '\0' && (line[i] == ' ' || line[i] == '\t'))
-		i++;
-	while (line[i] != '\0' && line[i] != ' ' && line[i] != '\t')
-	{
-		if (line[i] == LABEL_CHAR)
-			line[i] = ' ';
-		i++;
-	}
-    return (line);
-}
-
-char	**fill_tab(char *line, char **tab)
-{
-	int i;
-	int j;
-	int k;
-	int l;
-
-	i = 0;
-	j = 0;
-	while (line[i] != '\0' && line[i] != COMMENT_CHAR)
-	{
-		while (line[i] != '\0' && line[i] != COMMENT_CHAR && (line[i] == ' ' || line[i] == '\t'))
+		else
 			i++;
-		if (line[i] != '\0' && line[i] != COMMENT_CHAR)
+	}
+	return(tab);
+}
+
+char	**header_case(char *line, int count)
+{
+	char	**tab;
+	size_t	i;
+	size_t	j;
+	int		k;
+
+	if (!(tab = (char **)malloc(sizeof(char *) * (count + 1))))
+        clean_exit(NULL, "Memory allocation failed\n");
+	tab[count] = NULL;
+	i = 0;
+	k = 0;
+	while (k < count)
+	{
+		if (line[i] == '"')
 		{
-			if (line[i] == '"')
-			{
-				i++;
-				k = i;
-				while (line[k] != '"')
-					k++;
-				k -= i;
-				if (!(tab[j] = (char *)malloc(sizeof(char) * (k + 1))))
-					return (NULL);
-				l = 0;
-				while (l < k)
-					tab[j][l++] = line[i++];
-				tab[j++][l] = '\0';
-				i++;
-			}
-			else
-			{
-				k = i;
-				while (line[k] != '\0' && line[k] != COMMENT_CHAR && line[k] != '"' && line[k] != ' ' && line[k] != '\t')
-					k++;
-				k -= i;
-				if (!(tab[j] = (char *)malloc(sizeof(char) * (k + 1))))
-					return (NULL);
-				l = 0;
-				while (l < k)
-					tab[j][l++] = line[i++];
-				tab[j++][l] = '\0';
-			}
+			j = i + 1;
+			while (line[j] != '"')
+				j++;
+			if (!(tab[k] = ft_memalloc(j - i)))
+        		clean_exit(NULL, "Memory allocation failed\n");
+			tab[k] = ft_memcpy(tab[k], line + i + 1, j - i - 1);
+			k++;
+			i = j + 1;
 		}
+		else if (line[i] != ' ' && line[i] != '\t')
+		{
+			j = i;
+			while (line[j] != ' ' && line[j] != '\t' && line[j] != '"')
+				j++;
+			if (!(tab[k] = ft_memalloc(j - i + 1)))
+        		clean_exit(NULL, "Memory allocation failed\n");
+			tab[k] = ft_memcpy(tab[k], line + i, j - i);
+			k++;
+			i = j;
+		}
+		else
+			i++;
 	}
 	return (tab);
 }
 
-char    **split_line(char *line, int option)
+int		check_count_header(char *line)
 {
-    char    **tab;
-    int     count;
+	int i;
+	int count;
+	int quote;
+	int white_space;
 
-	if (line == NULL)
-		return(NULL);
-    if (option == 0)
-    {
-        count = check_quotemark(line);
-            if (count != 0 && count != 2)
-                return (NULL);
-    }
-    else if (option == 1)
-        line = prepare_op_line(line);
-	else
-		return (NULL);
-    count = count_tab(line);
-	if (count <= 1)
-		return (NULL);
-    if (!(tab = (char **)malloc(sizeof(char *) * (count + 1))))
-        return (NULL);
-    tab[count] = NULL;
-	tab = fill_tab(line, tab);
-	if (!tab)
-		return (NULL);
-    return(tab);
+	i = 0;
+	count = 0;
+	quote = 0;
+	white_space = 1;
+	while (line[i] != '\0' && (line[i] != COMMENT_CHAR || quote % 2))
+	{
+		if (line[i] == '"')
+		{
+			white_space = 1;
+			quote++;
+		}
+		else if (line[i] != ' ' && line[i] != '\t' && white_space)
+		{
+			count++;
+			white_space = 0;
+		}
+		else if ((line[i] == ' ' || line[i] == '\t') && !(quote % 2))
+			white_space = 1;
+		i++;
+	}
+	if (quote != 0 && quote != 2)
+		clean_exit(NULL, "Wrong number of quotes in file\n");
+	return (count);
 }
 
-/*
-int     main(int argc, char **argv)
+int    check_count_op(char *line)
 {
-    char **tab;
-	int i = 0;
+    int	i;
+	int	count;
+	int first_word;
+	int white_space;
 
-    (void)argc;
-    tab = split_line(argv[1], atoi(argv[2]));
-	if (tab)
+	i = 0;
+	count = 0;
+	first_word = 1;
+	white_space = 1;
+	while (line[i] != '\0' && line[i] != COMMENT_CHAR)
 	{
-		while (tab[i])
-			printf("%s\n", tab[i++]);
+		if (line[i] == ' ' || line[i] == '\t' || line[i] == SEPARATOR_CHAR)
+		{
+			white_space = 1;
+			if (count > 0)
+				first_word = 0;
+		}
+		else if (line[i] == LABEL_CHAR && first_word && line[i + 1] != ' '
+			&& line[i + 1] != '\t' && line[i + 1] != '\0' && line[i + 1] != COMMENT_CHAR)
+			count++;
+		else if (line[i] != ' ' && line[i] != '\t' && white_space)
+		{
+			count++;
+			white_space = 0;
+		}
+		i++;
 	}
-}*/
+    return (count);
+}
+
+char	**split_line(char *line, int option)
+{
+	int count;
+
+	if (line == NULL)
+		return (NULL);
+	if (option == 0)
+	{
+		count = check_count_header(line);
+		if (count == 0)
+			return (NULL);
+		return (header_case(line, count));
+	}
+	else if (option == 1)
+	{
+		count = check_count_op(line);
+		return (op_case(line, count));
+	}
+	else
+		return (NULL);
+}
