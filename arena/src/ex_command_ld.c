34bfc9a13baa5591cbaf868153bc05cd4d9fdf80
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-06 12:28:01 +0200

    norm

--- arena/src/ex_command_ld.c
+++ arena/src/ex_command_ld.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 18:47:19 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/06 12:13:44 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/06 12:24:05 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void			ex_command_ld(t_process *process, char *arena)
 {
-	t_command command;
-
-	int i;
-	int reg;
-	int jump;
+	t_command	command;
+	int			i;
+	int 		reg;
+	int			jump;
 
 	command = process->vm.command;
 	jump = command.op.type_needed ? 2 : 1;
@@ -28,10 +27,9 @@ void			ex_command_ld(t_process *process, char *arena)
 	command_extract_indirect(&command);
 	reg = command.reg_val[1].nb;
 	if (command.types[0].type == T_IND)
-		jump = command.ind_val[0].nb ;
+		jump = command.ind_val[0].nb;
 	jump %= IDX_MOD;
 	i = -1;
 	while (++i < DIR_SIZE)
 		process->registry[reg][i] = arena_val(arena, process->pc + i + jump);
-	printf("RUUUUUN COMMAND RUUUUUUUN !!!\n");
 }
