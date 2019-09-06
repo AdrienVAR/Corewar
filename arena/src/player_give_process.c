d89d3b213959195d94fb4b8184caf3f79cfb7fdb
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-06 16:52:18 +0200

    set r1 for players

--- arena/src/player_give_process.c
+++ arena/src/player_give_process.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 09:21:52 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/03 10:57:39 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/06 14:16:14 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ void		player_give_process(t_master *mstr)
 {
 	t_process	*process;
 	t_player	*player;
+	t_dir_cast	player_num;
 	int			i;
+	int			j;
 
 	i = -1;
 	while (++i < mstr->nb_of_players)
@@ -31,6 +33,10 @@ void		player_give_process(t_master *mstr)
 		player = mstr->players[i];
 		if (!(process = (t_process *)ft_memalloc(sizeof(t_process))))
 			exit_program(mstr);
+		player_num.nb = player->nb;
+		j = 0;
+		while (++j <= DIR_SIZE)
+			process->registry[0][j - 1] = player_num.casted[DIR_SIZE - j];
 		process->next = mstr->process;
 		process->pc = player->cursor_initial_pos;
 		mstr->process = process;
