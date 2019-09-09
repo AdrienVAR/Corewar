849d6a70768bb74f908293deb7a15fd4aeae0aaf
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-09 17:37:44 +0200

    memrevcpy replacement

--- arena/src/player_give_process.c
+++ arena/src/player_give_process.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 09:21:52 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/06 14:16:14 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/09 17:27:45 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void		player_give_process(t_master *mstr)
 	t_player	*player;
 	t_dir_cast	player_num;
 	int			i;
-	int			j;
 
 	i = -1;
 	while (++i < mstr->nb_of_players)
@@ -34,9 +33,7 @@ void		player_give_process(t_master *mstr)
 		if (!(process = (t_process *)ft_memalloc(sizeof(t_process))))
 			exit_program(mstr);
 		player_num.nb = player->nb;
-		j = 0;
-		while (++j <= DIR_SIZE)
-			process->registry[0][j - 1] = player_num.casted[DIR_SIZE - j];
+		memrevcpy(process->registry[0], player_num.casted, DIR_SIZE);
 		process->next = mstr->process;
 		process->pc = player->cursor_initial_pos;
 		mstr->process = process;
