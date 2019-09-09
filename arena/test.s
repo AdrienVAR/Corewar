05589ffd0b1f5a77c039bbc56b656d642794e5ca
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-09 16:30:51 +0200

    new functions tested

--- arena/test.s
+++ arena/test.s
@@ -1,10 +1,13 @@
 .name "test"
 .comment "nope"
 
-st r1, r6
-st r1, 6
-live %2
-ld %1, r10
-ld 7, r5
-ld %20, r4
-ld -17, r5
+and r1, r1, r1
+ld %6, r3
+ld %4, r4
+ld %15, r8
+ld %255, r7
+or %256, -5 ,r8
+or r1, %254 ,r8
+xor %7, %27 ,r8
+and r3, r4, r5
+and r2, r2, r2
