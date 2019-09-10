41c65bf99f30b8f5dcf38b27f45dc97956311d3e
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-10 15:59:57 +0200

    ldi and sti

--- arena/test.s
+++ arena/test.s
@@ -1,13 +1,6 @@
 .name "test"
 .comment "nope"
 
-and r1, r1, r1
-ld %6, r3
-ld %4, r4
-ld %15, r8
-ld %255, r7
-or %256, -5 ,r8
-or r1, %254 ,r8
-xor %7, %27 ,r8
-and r3, r4, r5
-and r2, r2, r2
+label: ldi 7, %11, r3
+ld %-2, r5
+sti r3, -5, r5
