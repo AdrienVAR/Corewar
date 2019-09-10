028cf6cbf2fd105e1ab65c1e53411a6c991038da
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-10 09:44:08 +0200

    norm

--- arena/src/cursor_next_op.c
+++ arena/src/cursor_next_op.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 15:41:18 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/10 09:30:44 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/10 09:43:02 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void		cursor_next_op(t_process *process)
 {
-	int 		i;
+	int			i;
 	int			pc;
 	t_command	command;
 
 	command = process->vm.command;
 	pc = process->pc;
-	if(command.op.id == 9)
+	if (command.op.id == 9)
 		return ;
 	else if (!command.op.type_needed)
 		pc += 1 + DIR_SIZE;
