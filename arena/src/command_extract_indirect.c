0b5e7178a724b393e4503f330e58e252f819f139
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-10 15:58:02 +0200

    management of dir to ind size conversion

--- arena/src/command_extract_indirect.c
+++ arena/src/command_extract_indirect.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 09:52:51 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/09 16:07:11 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/10 14:58:18 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ void		command_extract_indirect(t_command *command)
 	while (++p_ind < MAX_ARGS_NUMBER)
 	{
 		command->ind_val[p_ind].nb = 0;
-		if (command->types[p_ind].type != T_IND)
+		if (command->types[p_ind].type != T_IND
+			&& !(command->types[p_ind].type == T_DIR &&
+				command->op.dir_size == IND_SIZE))
 			continue;
 		memrevcpy(command->ind_val[p_ind].casted,
 		command->param[p_ind], IND_SIZE);
