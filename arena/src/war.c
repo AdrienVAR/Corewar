83d65bf2896061e64bf85ed5d881e3abfd6d36a8
Cizeur <cgiron@e1r5p7.42.fr>
2019-09-11 18:51:59 +0200

    start of foamy bat implementation and management of fork

--- arena/src/war.c
+++ arena/src/war.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 14:07:46 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/10 15:55:45 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/11 18:07:54 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@
 **		**************************************************
 */
 
-void			run_command(t_process *cur_process, char *arena)
+void			run_command(t_master *mstr, t_process *cur_process, char *arena)
 {
 	int				op_id;
 	static void		(*ex_command[AVAILABLE_OPERATIONS])() = {0,
 		&ex_command_live, &ex_command_ld, &ex_command_st, &ex_command_add
 		, &ex_command_sub, &ex_command_and, &ex_command_or, &ex_command_xor
-		, ex_command_zjmp, ex_command_ldi, ex_command_sti};
+		, ex_command_zjmp, ex_command_ldi, ex_command_sti, 0};
 
 	op_id = cur_process->vm.command.op.id;
-	if (op_id && op_id <= 11)
+	if (op_id == 12)
+		ex_command_fork(mstr, cur_process, arena);
+	else if (op_id && op_id <= 11)
 		ex_command[op_id](cur_process, arena);
 	cursor_next_op(cur_process);
 }
@@ -43,7 +45,7 @@ void			run_cycle(t_master *mstr)
 	while (cur_process)
 	{
 		if (cur_process->vm.wait == 1)
-			run_command(cur_process, arena);
+			run_command(mstr, cur_process, arena);
 		else if (!cur_process->vm.wait)
 		{
 			if (command_get_info(cur_process, arena_val(arena, cur_process->pc)) == YES)
@@ -67,12 +69,46 @@ void			run_cycle(t_master *mstr)
 	}
 }
 
+int			kill_processes(t_master *mstr, t_process **process)
+{
+	t_process *prev;
+	t_process *cur;
+	t_process *next;
+	int test;
+
+	prev = 0;
+	cur = *process;
+	next = cur->next;
+	while (cur)
+	{
+		test = cur->vm.process_nb;
+		if (cur->vm.last_live > mstr->foamy_bat_cycle)
+		{
+			ft_bzero(cur, sizeof(t_process));
+			ft_memdel((void **)&cur);
+			if (!prev)
+				*process = next;
+			else
+				prev->next = next;
+		}
+		prev = cur ? cur : prev;
+		cur = next;
+		next = cur ? cur->next : next;
+	}
+	return (mstr->foamy_bat_cycle);
+}
+
 void			war(t_master *mstr)
 {
+	int		ctd;
 
-	while (mstr->cur_cycle < 50000)
+	ctd = mstr->foamy_bat_cycle;
+	while (mstr->cur_cycle < 20000)
 	{
 		++mstr->cur_cycle;
+		ctd--;
 		run_cycle(mstr);
+		if (!ctd)
+			ctd = kill_processes(mstr, &mstr->process);
 	}
 }
