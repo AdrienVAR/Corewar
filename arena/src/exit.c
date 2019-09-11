03e8ea349fa0e8cdadb340c344fffbe002cca2d0
Cizeur <cgiron@e1r5p7.42.fr>
2019-09-11 18:50:05 +0200

    clean every malloc

--- arena/src/exit.c
+++ arena/src/exit.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 12:14:37 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/03 16:06:44 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/11 18:45:13 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,37 @@
 **		***************************
 */
 
+void		free_player(t_player **player)
+{
+	int i;
+
+	i = -1;
+	while (++i < MAX_PLAYERS)
+	{
+		ft_bzero(player[i], sizeof(t_player));
+		ft_memdel((void **)&player[i]);
+	}
+}
+
+void		free_process(t_process *process)
+{
+	t_process *stock;
+
+	while (process)
+	{
+		stock = process->next;
+		ft_bzero(process, sizeof(process));
+		ft_memdel((void **)&process);
+		process = stock;
+	}
+}
+
 void		free_everything(t_master *mstr)
 {
 	if (mstr)
 	{
+		free_player(&(mstr->players[0]));
+		free_process(mstr->process);
 		ft_bzero(mstr, sizeof(t_master));
 		free(mstr);
 	}
