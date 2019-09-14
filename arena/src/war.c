289ef55ac04de3f2e61df661b920f94788e1354b
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-14 20:32:08 +0200

    revision of ex_command struct plus new functions

--- arena/src/war.c
+++ arena/src/war.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 14:07:46 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/12 15:50:04 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/14 20:11:34 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,20 @@ void			run_command(t_master *mstr, t_process *cur_process, char *arena)
 	static void		(*ex_command[AVAILABLE_OPERATIONS])() = {0,
 		&ex_command_live, &ex_command_ld, &ex_command_st, &ex_command_add
 		, &ex_command_sub, &ex_command_and, &ex_command_or, &ex_command_xor
-		, ex_command_zjmp, ex_command_ldi, ex_command_sti, 0};
+		, ex_command_zjmp, ex_command_ldi, ex_command_sti, &ex_command_fork,
+		&ex_command_lld, &ex_command_lldi, ex_command_lfork};
 
 	op_id = cur_process->vm.command.op.id;
-	if (op_id == 12)
-		ex_command_fork(mstr, cur_process, arena);
-	else if (op_id && op_id <= 11)
-		ex_command[op_id](cur_process, arena);
+	if (command_valid_types(cur_process->vm.command) == YES &&
+		command_convert_param(cur_process, arena) == YES)
+	{
+		printf("command en cours : %s\n", cur_process->vm.command.op.name);
+		if (op_id && op_id < AVAILABLE_OPERATIONS)
+			ex_command[op_id](mstr, cur_process, arena);
+	}
 	cursor_next_op(cur_process);
-	if (cur_process->vm.alive >= 1
-		&& cur_process->vm.alive <= mstr->nb_of_players)
-		{
-			mstr->last_player_live = cur_process->vm.alive;
-			mstr->live_signal++;
-		}
-	if (cur_process->vm.last_live == -1)
-			mstr->live_signal++;
 	cur_process->vm.alive = 0;
+	ft_bzero(&(cur_process->vm.command), sizeof(t_command));
 }
 
 void			run_cycle(t_master *mstr)
@@ -54,14 +51,15 @@ void			run_cycle(t_master *mstr)
 	while (cur_process)
 	{
 		if (cur_process->vm.wait == 1)
+		{
+			command_get_types(cur_process, cur_process->pc, arena);
+			command_get_param(cur_process, arena);
 			run_command(mstr, cur_process, arena);
+		}
 		else if (!cur_process->vm.wait)
 		{
-			if (command_get_info(cur_process, arena_val(arena, cur_process->pc)) == YES)
-			{
-				command_get_types(cur_process, arena_val(arena, cur_process->pc + 1));
-				command_get_param(cur_process, arena);
-			}
+			if (command_get_info(cur_process, cur_process->pc, arena) == YES)
+				;
 			else
 			{
 				cur_process->vm.wait++;
@@ -119,6 +117,9 @@ void			war(t_master *mstr)
 	ctd = mstr->foamy_bat_cycle;
 	while (mstr->process)
 	{
+		if (mstr->cur_cycle >= mstr->options.dump
+			&& mstr->options.end_dump == N_DUMP)
+			break ;
 		++mstr->cur_cycle;
 		ctd--;
 		run_cycle(mstr);
