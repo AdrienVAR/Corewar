a37d17f42d93101853132515ca22d43c88bf475f
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-14 20:30:56 +0200

    use of uchar

--- arena/src/memory_dump.c
+++ arena/src/memory_dump.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 15:26:23 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/03 11:54:35 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/14 09:43:00 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void			memory_dump(t_master *mstr)
 {
 	char		*arena;
+	char		byte;
 	static char hexa[17] = "0123456789abcdef";
 	int			i;
 
@@ -23,10 +24,11 @@ void			memory_dump(t_master *mstr)
 	i = -1;
 	while (++i < MEM_SIZE)
 	{
+		byte = arena_val_get(arena, i);
 		if (i && !(i % DUMP_SIZE))
 			ft_putchar('\n');
-		ft_putchar(hexa[((unsigned char)arena[i] / 16)]);
-		ft_putchar(hexa[((unsigned char)arena[i] % 16)]);
+		ft_putchar(hexa[(t_uchar)byte / 16]);
+		ft_putchar(hexa[(t_uchar)byte % 16]);
 		ft_putchar(' ');
 	}
 	ft_putchar('\n');
