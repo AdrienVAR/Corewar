bd87eef717569b73a0478d73cdbd1bee987fab7a
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-18 17:27:44 +0200

    reduction of print buffer

--- libft/includes/ft_printf.h
+++ libft/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 07:50:58 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/18 08:41:16 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/18 16:01:24 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <wchar.h>
 # include <stdarg.h>
 
-# define BUFFER 1024
+# define BUFFER 512
 # define CONVERSION "%idspuoxXUcCSDOfFeEgG~bBrkn"
 # define OPTION_LST "-+#0 .123456789hlLjz*"
 # define SIZE_LST "hlLjz"
@@ -54,7 +54,7 @@
 ** fringe [2] 1 si str && strl sont nuls 0 sinon;
 */
 
-typedef struct	s_master
+typedef struct	s_printf
 {
 	char		flt_str[MAX_STR_SZ];
 	char		*shuttle;
@@ -66,52 +66,52 @@ typedef struct	s_master
 	int			prefix[2];
 	int			error;
 	int			returnval;
-}				t_master;
+}				t_printf;
 
 int				ft_printf(const char *restrict format, ...);
 int				ft_sprintf(char *str, const char *restrict format, ...);
 int				ft_asprintf(char **str, const char *restrict format, ...);
 int				ft_dprintf(int fd, const char *restrict format, ...);
-void			ft_buffer_grow(t_master *mstr);
-void			ft_update_buffer(t_master *mstr, char c);
+void			ft_buffer_grow(t_printf *mstr);
+void			ft_update_buffer(t_printf *mstr, char c);
 void			ft_buffer_del(t_list **buffer);
-void			ft_list_print(t_list *buffer, t_master *mstr);
-void			ft_list_sprint(char *str, t_list *buffer, t_master *mstr);
-void			ft_list_asprint(char **str, t_list *buffer, t_master *mstr);
-void			ft_list_dprint(int fd, t_list *buffer, t_master *mstr);
-void			ft_flag_mngmt(t_master *mstr);
-char			*ft_flag_option_check(char *navette, t_master *mstr);
-void			ft_init_master(t_master *mstr, const char *restrict format);
-intmax_t		ft_fetch_int(t_master *mstr);
-uintmax_t		ft_fetch_ptr(t_master *mstr);
-uintmax_t		ft_fetch_time(t_master *mstr);
-void			ft_fetch_str(t_master *mstr, wchar_t **strl, char **str);
-void			ft_fetch_char(t_master *mstr, wint_t *chrl, char *chr);
-void			ft_fetch_flt(t_master *mstr, double *dbl, long double *ldbl);
-uintmax_t		ft_fetch_u_int(t_master *mstr);
-intmax_t		*ft_fetch_int_adr(t_master *mstr);
-void			ft_conv_int(t_master *mstr);
-void			ft_conv_percent(t_master *mstr);
-void			ft_conv_str(t_master *mstr);
-void			ft_conv_np_str(t_master *mstr);
-void			ft_conv_ptr(t_master *mstr);
-void			ft_conv_char(t_master *mstr);
-void			ft_conv_u_int(t_master *mstr);
-void			ft_conv_flt(t_master *mstr);
-void			ft_conv_flt_e(t_master *mstr);
-void			ft_conv_flt_g(t_master *mstr);
-void			ft_conv_time(t_master *mstr);
-void			ft_conv_color(t_master *mstr);
-void			ft_conv_n(t_master *mstr);
+void			ft_list_print(t_list *buffer, t_printf *mstr);
+void			ft_list_sprint(char *str, t_list *buffer, t_printf *mstr);
+void			ft_list_asprint(char **str, t_list *buffer, t_printf *mstr);
+void			ft_list_dprint(int fd, t_list *buffer, t_printf *mstr);
+void			ft_flag_mngmt(t_printf *mstr);
+char			*ft_flag_option_check(char *navette, t_printf *mstr);
+void			ft_init_printf(t_printf *mstr, const char *restrict format);
+intmax_t		ft_fetch_int(t_printf *mstr);
+uintmax_t		ft_fetch_ptr(t_printf *mstr);
+uintmax_t		ft_fetch_time(t_printf *mstr);
+void			ft_fetch_str(t_printf *mstr, wchar_t **strl, char **str);
+void			ft_fetch_char(t_printf *mstr, wint_t *chrl, char *chr);
+void			ft_fetch_flt(t_printf *mstr, double *dbl, long double *ldbl);
+uintmax_t		ft_fetch_u_int(t_printf *mstr);
+intmax_t		*ft_fetch_int_adr(t_printf *mstr);
+void			ft_conv_int(t_printf *mstr);
+void			ft_conv_percent(t_printf *mstr);
+void			ft_conv_str(t_printf *mstr);
+void			ft_conv_np_str(t_printf *mstr);
+void			ft_conv_ptr(t_printf *mstr);
+void			ft_conv_char(t_printf *mstr);
+void			ft_conv_u_int(t_printf *mstr);
+void			ft_conv_flt(t_printf *mstr);
+void			ft_conv_flt_e(t_printf *mstr);
+void			ft_conv_flt_g(t_printf *mstr);
+void			ft_conv_time(t_printf *mstr);
+void			ft_conv_color(t_printf *mstr);
+void			ft_conv_n(t_printf *mstr);
 int				ft_int_size_base(intmax_t nb, int base_sz);
 int				ft_u_int_size_base(intmax_t nb, int base_sz);
-void			ft_buffer_digits_base(t_master *mstr,
+void			ft_buffer_digits_base(t_printf *mstr,
 		uintmax_t nb, char *base);
-void			ft_prefixes(t_master *mstr, int prefix);
+void			ft_prefixes(t_printf *mstr, int prefix);
 int				ft_unichar_length(wchar_t uni);
-void			ft_add_uni_buffer(t_master *mstr, wchar_t uni);
-void			ft_standard_padding(t_master *mstr, char c);
+void			ft_add_uni_buffer(t_printf *mstr, wchar_t uni);
+void			ft_standard_padding(t_printf *mstr, char c);
 int				ft_prec_zero_pref(int type);
-void			ft_str_to_buffer(t_master *mstr, char *str);
+void			ft_str_to_buffer(t_printf *mstr, char *str);
 
 #endif
