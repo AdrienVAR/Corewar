ffcce45753f63ad1ca106433800b3d8aa7be933c
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-18 17:21:46 +0200

    new aff function

---
+++ arena/src/ex_command/ex_command_aff.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex_command_aff.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/04 18:47:19 by cgiron            #+#    #+#             */
+/*   Updated: 2019/09/18 17:09:48 by cgiron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "arena.h"
+#include "libft/ft_printf.h"
+
+static void			aff_verbose(t_process *process)
+{
+	ft_printf("P - %5d | op : %5s %c\n",
+		process->vm.process_nb,
+		process->vm.command.op.name,
+		process->vm.command.param_ext_conv[0].nb);
+}
+
+void			ex_command_aff(t_master *mstr, t_process *process, char *arena)
+{
+	(void)mstr;
+	(void)arena;
+	aff_verbose(process);
+	ft_printf("%c\n", process->vm.command.param_ext_conv[0].nb % 256);
+}
