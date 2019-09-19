c21da91eac37b6baf237fcfcead4d4ce9a80cb3e
Cizeur <cgiron@e1r5p7.42.fr>
2019-09-19 15:46:52 +0200

    back to debug for now

--- shared_val.mk
+++ shared_val.mk
@@ -6,7 +6,7 @@
 #    By: cizeur <cizeur@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/19 09:36:56 by cgiron            #+#    #+#              #
-#    Updated: 2019/09/18 21:54:49 by cizeur           ###   ########.fr        #
+#    Updated: 2019/09/19 15:46:29 by cgiron           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,7 +25,7 @@ vpath %.o $(SHARE_OBJ_DIR)
 vpath %.d $(SHARE_OBJ_DIR)
 
 SHARE_FLAGS				:= -Wall -Wextra -Werror
-OPTI_FLAGS				:= -O3 -ffreestanding -fno-builtin -flto
-##OPTI_FLAGS				:= -g
+#OPTI_FLAGS				:= -O3 -ffreestanding -fno-builtin -flto
+OPTI_FLAGS				:= -g
 SHARE_FLAGS				+= -I $(SHARE_INC_DIR)
 SHARE_DEBUG				:= -g3
