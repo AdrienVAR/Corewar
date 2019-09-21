9bcf12d71fc5a4c5b4b2eafa14b3d8e18d3602e3
Cesar Giron <cizeur@Mac-mini-de-Cesar.local>
2019-09-21 22:01:38 +0200

    memory dump sqme format as zaz

--- arena/src/memory_dump.c
+++ arena/src/memory_dump.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   memory_dump.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cizeur <cizeur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 15:26:23 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/14 09:43:00 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/21 19:11:39 by cizeur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arena.h"
 #include "libft/libft.h"
+#include "libft/ft_printf.h"
 
 void			memory_dump(t_master *mstr)
 {
@@ -22,11 +23,12 @@ void			memory_dump(t_master *mstr)
 
 	arena = mstr->arena;
 	i = -1;
+	ft_printf("0x0000 : ", 0);
 	while (++i < MEM_SIZE)
 	{
 		byte = arena_val_get(arena, i);
 		if (i && !(i % DUMP_SIZE))
-			ft_putchar('\n');
+			ft_printf("\n%#.4x : ", i);
 		ft_putchar(hexa[(t_uchar)byte / 16]);
 		ft_putchar(hexa[(t_uchar)byte % 16]);
 		ft_putchar(' ');
