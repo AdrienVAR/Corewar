5b8ad371bfa4b68e31fa4bce8f30a44c7163f66f
Cesar Giron <cizeur@Mac-mini-de-Cesar.local>
2019-09-21 17:21:30 +0200

    verbose player live implemented

--- arena/src/ex_command/ex_command_live.c
+++ arena/src/ex_command/ex_command_live.c
@@ -6,7 +6,7 @@
 /*   By: cizeur <cizeur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 18:47:19 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/20 19:59:29 by cizeur           ###   ########.fr       */
+/*   Updated: 2019/09/21 17:14:44 by cizeur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,15 @@ static void		live_verbose(t_process *process)
 		process->vm.command.param_conv[0].nb);
 }
 
+static void		player_live_verbose(t_master *mstr)
+{
+	if (!(mstr->options.verbose & VERBOSE_LIFE))
+		return;
+	ft_printf("Player %d : %s squeezed in a live signal\n",
+		mstr->last_player_live,
+		mstr->players[mstr->last_player_live - 1]->name);
+}
+
 void			ex_command_live(t_master *mstr, t_process *process, char *arena)
 {
 	t_command	command;
@@ -28,12 +37,15 @@ void			ex_command_live(t_master *mstr, t_process *process, char *arena)
 	(void)arena;
 	command = process->vm.command;
 	process->vm.last_live = -1;
-	process->vm.alive = command.param_conv[0].nb;
+	process->vm.alive = -command.param_conv[0].nb;
 	if (mstr->options.verbose & VERBOSE_OPER)
 		live_verbose(process);
 	if (process->vm.alive >= 1
 		&& process->vm.alive <= mstr->nb_of_players)
-		mstr->last_player_live = process->vm.alive;
+		{
+			mstr->last_player_live = process->vm.alive;
+			player_live_verbose(mstr);
+		}
 	process->vm.alive = 0;
 	mstr->live_signal++;
 }
