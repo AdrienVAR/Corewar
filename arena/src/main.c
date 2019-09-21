8dc23685d97ee5dcd44d08b855c3b26ec6203174
Cesar Giron <cizeur@Mac-mini-de-Cesar.local>
2019-09-21 22:00:59 +0200

    main now announce winner

--- arena/src/main.c
+++ arena/src/main.c
@@ -3,15 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cizeur <cizeur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 09:59:05 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/19 15:04:30 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/21 18:01:21 by cizeur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "arena.h"
+#include "libft/ft_printf.h"
+
+static void			print_winner_name(t_player *player)
+{
+	ft_printf("Player %3.0~%d%~ : %3.0~%s%~ WON\n", player->nb, player->name);
+	ft_putstr("He was the last alive (that i noticed)\n");
+	ft_printf("		%3.0~CONGRATS%~\n");
+}
 
 int				main(int argc, char **argv)
 {
@@ -30,8 +38,9 @@ int				main(int argc, char **argv)
 	arena_populate(mstr);
 	player_give_process(mstr);
 	war(mstr);
+	print_winner_name(mstr->players[mstr->last_player_live - 1]);
 	if (mstr->options.end_dump)
 		memory_dump(mstr);
-	exit_program(mstr);
+	free_everything(mstr);
 	return (0);
 }
