563be967d850964115f8d9a0da20c8c5db48ec67
Cizeur <cgiron@e3r3p1.42.fr>
2019-09-23 19:15:59 +0200

    implementation visu

--- arena/src/main.c
+++ arena/src/main.c
@@ -6,20 +6,13 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 09:59:05 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/23 10:15:37 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/23 14:05:45 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "arena.h"
 #include "libft/ft_printf.h"
-
-static void		print_winner_name(t_player *player)
-{
-	ft_printf("Player %3.0~%d%~ : %3.0~%s%~ WON\n", player->nb, player->name);
-	ft_putstr("He was the last alive (that i noticed)\n");
-	ft_printf("		%3.0~CONGRATS%~\n");
-}
 
 int				main(int argc, char **argv)
 {
@@ -37,9 +30,9 @@ int				main(int argc, char **argv)
 	file_closing(mstr);
 	arena_populate(mstr);
 	player_give_process(mstr);
-	war(mstr);
+	mstr->options.visu == YES ? visu_corewar(mstr) : war(mstr);
 	if (mstr->options.end_dump != N_DUMP)
-		print_winner_name(mstr->players[mstr->last_player_live - 1]);
+		print_winner(mstr->players[mstr->last_player_live - 1]);
 	if (mstr->options.end_dump)
 		memory_dump(mstr);
 	free_everything(mstr);
