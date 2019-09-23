b25e074703a3ed0cd3ee685486f10f125751d662
Cizeur <cgiron@e3r3p1.42.fr>
2019-09-23 19:13:42 +0200

    rename sortie program

---
+++ arena/src/exit_program.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_program.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/31 12:14:37 by cgiron            #+#    #+#             */
+/*   Updated: 2019/09/23 14:17:59 by cgiron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "arena.h"
+#include "libft/libft.h"
+#include <stdlib.h>
+
+/*
+**		***************************
+** **	Provide clean exit on ERROR ** **
+**		***************************
+*/
+
+void			exit_program(t_master *mstr)
+{
+	ft_putstr("ERROR\n");
+	free_everything(mstr);
+	exit(1);
+}
