a873ef5c27a97a50ad68c8dd34de390f173bc5a5
Cizeur <cgiron@e3r3p1.42.fr>
2019-09-23 19:16:43 +0200

    ajout des player nb pour les couleurs

--- arena/src/arena_populate.c
+++ arena/src/arena_populate.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 13:28:23 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/14 20:07:35 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/23 17:23:48 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,16 @@ void		arena_populate(t_master *mstr)
 	i = -1;
 	arena = &(mstr->arena[0]);
 	step = MEM_SIZE / mstr->nb_of_players;
+	ft_memset(mstr->col_arena, MAX_PLAYERS + 1, MEM_SIZE);
 	while (++i < mstr->nb_of_players)
 	{
 		player = mstr->players[i];
 		j = -1;
 		while (++j < player->code_size)
+		{
 			arena[(i * step + j) % MEM_SIZE] = player->exec[j];
+			mstr->col_arena[(i * step + j) % MEM_SIZE] = player->nb;
+		}
 		player->cursor_initial_pos = (i) * step;
 	}
 }
