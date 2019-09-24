76e1838be0004a1ab690c6c556fa66c3a9a4fb89
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-24 17:39:28 +0200

    added colors protos layout defines and update state overall changed

--- arena/includes/visu_arena.h
+++ arena/includes/visu_arena.h
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 11:50:53 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/23 18:47:16 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/24 17:36:52 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,44 @@
 
 # define YRES 1080
 # define XRES 1920
-# define TOP_Y 100
-# define BOTTOM_Y 100
-# define LEFT_X 50
+
+# define TOP_Y 150
+# define STD_BUF 25
+# define STD_NEW_LINE 22
+# define BOTTOM_Y STD_BUF
+# define LEFT_X STD_BUF
+# define RIGHT_X STD_BUF
+
+
+# define RIGHT_BAR_SIZE 500
+# define RIGHT_BAR_X (XRES - RIGHT_X - RIGHT_BAR_SIZE)
+# define ARENA_X (RIGHT_BAR_X - LEFT_X)
+# define ARENA_Y (YRES - (TOP_Y + BOTTOM_Y))
+
+# define BYTE_X (ARENA_X / 64)
+# define BYTE_Y (ARENA_Y / (MEM_SIZE / 64))
 
 # define SPACE 49
 # define ESCAPE 53
+# define TOUCH_P 35
+# define TOUCH_UP 126
+# define TOUCH_DOWN 125
+# define TOUCH_M 46
 
-# define MLX_PTR 0
-# define WIN_PTR 1
-# define I_PTR 2
-# define I_ADR 3
+# define NO_CURSOR 1
+# define CURSOR_PRESENT 2
+# define CURSOR_ACTIVE 3
 
+# define I_PTR 0
+# define I_ADR 1
+# define MLX_PTR 2
+# define WIN_PTR 3
+
+
+# define COLOR_BACKGROUND 0x1F1F1F
 # define COLOR_BASIC 0xFFFFFF
+# define COLOR_CURSOR 0x8F8F8F
+# define COLOR_CURSOR_ACT 0xAFAFAF
 # define VISU_DIM 80
 # define P1_COL 0x00FFFF
 # define P2_COL 0x00FF00
@@ -39,18 +64,25 @@ typedef enum		e_update
 	U_IDLE,
 	U_END,
 	U_STOP,
-	U_ONE_TURN
+	U_ONE_TURN,
+	U_TILL_THE_END,
+	U_REFRESH,
+	U_MULTI_TURN,
 }					t_update;
 
 typedef struct	s_visu
 {
 	void		*mem_ptr[4];
+	void		*header;
+	int			header_size[2];
 	t_update	update;
+	int			multi;
 }				t_visu;
 
 void			clear_window(t_visu *visu);
 int				key_simple_press(int key, t_visu *visu);
 int				key_loop(int key, t_visu *visu);
-void			reset_screen(void **mem_ptr);
+void			flush_image(t_visu *visu);
+void			draw_pixel(t_visu *visu, int x, int y, int color);
 
 #endif
