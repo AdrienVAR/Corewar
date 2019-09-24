20daa9a8950c110af53746638ba21fef131b2e6f
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-24 17:29:17 +0200

    able to manage simple as well a multi turn

--- arena/src/visu/key_loop.c
+++ arena/src/visu/key_loop.c
@@ -6,18 +6,26 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 11:23:25 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/23 19:01:56 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/24 14:51:46 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arena.h"
 #include "visu_arena.h"
+#include "utils.h"
 
 # include <stdio.h>
 
 int		key_loop(int key, t_visu *visu)
 {
 	visu->update = key == SPACE ? U_ONE_TURN : visu->update;
-	printf("touch pressee %d et update %d\n", key, visu->update);
+	visu->update = key == TOUCH_M ? U_MULTI_TURN : visu->update;
+	visu->multi = key == TOUCH_UP ?
+		multi_turn_selector(visu->multi, 1) : visu->multi;
+	visu->multi = key == TOUCH_DOWN ?
+		multi_turn_selector(visu->multi, 0) : visu->multi;
+	if (key == TOUCH_UP || key == TOUCH_DOWN)
+		visu->update = visu->update == U_IDLE ? U_REFRESH : visu->update;
+	//printf("touch pressee %d et update %d\n", key, visu->update);
 	return (key);
 }
