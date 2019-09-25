1b399844d9b1a8cd1c240613e3407e9c46847e9b
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-25 12:30:11 +0200

    changed to accomodate no winners

--- arena/src/print_winner.c
+++ arena/src/print_winner.c
@@ -6,16 +6,21 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 14:02:45 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/23 14:03:49 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/25 12:22:24 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arena.h"
 #include "libft/ft_printf.h"
 
-void		print_winner(t_player *player)
+void		print_winner(t_player *player, int someone_lived)
 {
-	ft_printf("Player %3.0~%d%~ : %3.0~%s%~ WON\n", player->nb, player->name);
-	ft_putstr("He was the last alive (that i noticed)\n");
-	ft_printf("		%3.0~CONGRATS%~\n");
+	if (someone_lived)
+	{
+		ft_printf("Player %3.0~%d%~ : %3.0~%s%~ WON\n", player->nb, player->name);
+		ft_putstr("He was the last alive (that i noticed)\n");
+		ft_printf("		%3.0~CONGRATS%~\n");
+	}
+	else
+		ft_putstr("No one looked alive to me : \e[31mNO WINNER\e[0m\n");
 }
