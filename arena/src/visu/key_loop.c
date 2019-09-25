511126dafaa61c75a696ae71b3bd8bcc246fdbc0
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-25 12:34:02 +0200

    added playy till something happens

--- arena/src/visu/key_loop.c
+++ arena/src/visu/key_loop.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 11:23:25 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/24 14:51:46 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/25 10:20:40 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ int		key_loop(int key, t_visu *visu)
 {
 	visu->update = key == SPACE ? U_ONE_TURN : visu->update;
 	visu->update = key == TOUCH_M ? U_MULTI_TURN : visu->update;
+	visu->update = key == TOUCH_A ? U_TILL_SOMETHING_HAPPEN : visu->update;
 	visu->multi = key == TOUCH_UP ?
 		multi_turn_selector(visu->multi, 1) : visu->multi;
 	visu->multi = key == TOUCH_DOWN ?
 		multi_turn_selector(visu->multi, 0) : visu->multi;
 	if (key == TOUCH_UP || key == TOUCH_DOWN)
 		visu->update = visu->update == U_IDLE ? U_REFRESH : visu->update;
-	//printf("touch pressee %d et update %d\n", key, visu->update);
+	printf("touch pressee %d et update %d\n", key, visu->update);
 	return (key);
 }
