48ee151f1b9beb1c282633e8af20aef28163b944
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-25 18:42:18 +0200

    corrected color inversion

--- arena/src/visu/draw_pixel_image.c
+++ arena/src/visu/draw_pixel_image.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 10:43:45 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/24 10:51:42 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/25 18:18:27 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void			draw_pixel(t_visu *visu, int x, int y, int color)
 	img_cont = (visu->mem_ptr)[I_ADR];
 	if (x < XRES && y < YRES && x >= 0 && y >= 0)
 	{
-		img_cont[4 * (x + y * XRES) + 2] = color % 256;
+		img_cont[4 * (x + y * XRES) + 2] = color / (256 * 256);
 		img_cont[4 * (x + y * XRES) + 1] = (color / 256) % 256;
-		img_cont[4 * (x + y * XRES) + 0] = color / (256 * 256);
+		img_cont[4 * (x + y * XRES) + 0] = color % 256;
 	}
 }
