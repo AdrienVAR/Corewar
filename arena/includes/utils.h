33d86f67c32088625cccff1b51c4f8a412a3fb89
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-25 12:40:28 +0200

    added some protos

--- arena/includes/utils.h
+++ arena/includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 16:59:45 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/24 13:01:06 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/25 09:15:55 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int			is_integer(char *line);
 void		*memrevcpy(void *dst, const void *src, int n);
 int			rgb(unsigned char r, unsigned char g, unsigned char b);
 int			multi_turn_selector(int mult, int plusmin);
+int			color_dimmer(int color, int dim);
 
 #endif
