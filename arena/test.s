77972d8e1ecbe2c7c63fc69573b58059cd38b133
Cizeur <cgiron@e3r4p1.42.fr>
2019-09-26 11:20:53 +0200

    champion in progress

--- arena/test.s
+++ arena/test.s
@@ -1,12 +1,26 @@
-.name "test"
+.name "tes545454545454545454545454545454545454545454545554t"
 .comment "nope"
 
 
-alive: 
-live %42
 zjmp %:end
-and %0, %0, r16
-fork %:alive
-wat: live %42
-fork %:wat
+ld %1, r15
+st r15, -4
+st r1, -10
+ld %1, r3
+alive:
+	st r1, 6
+	live %42
+	add r3, r15 ,r3
+	fork %:alive
+	sub r3, r15  ,r3
+loop_init:
+	st r3, r4
+	ld %1, r15
+	st r1, 6
+loop:
+	live %42
+	sub r4, r15, r4
+	zjmp %:alive
+	and %0, %0, r12
+	zjmp %:loop
 end:
