d8e672241d51d6ed65619a7e95808999a08d2ab2
Cizeur <cgiron@e2r10p9.42.fr>
2019-09-26 14:00:01 +0200

    test asm

--- arena/test.s
+++ arena/test.s
@@ -2,25 +2,58 @@
 .comment "nope"
 
 
-zjmp %:end
-ld %1, r15
-st r15, -4
-st r1, -10
-ld %1, r3
+#Have enemy skip  
+
+or			%0,		%1,		r16
+zjmp		%:endless
+st			r2		,		-8	
+
+#Turn start into load my player nb in your r1 
+
+start:
+
+	ld		%1,		r15
+	st 		r15,	-4
+	st		r1,		-10
+
+#The goal is to achieve r8 forks before life signaling
+
+init_imb:
+
+	ld 		%5,		r8				#sets the number of forks
+	add		r3,		r15,		r3	#incrementation of next loop nb
+	st		r1,		6
+
 alive:
-	st r1, 6
-	live %42
-	add r3, r15 ,r3
-	fork %:alive
-	sub r3, r15  ,r3
+	live	%42
+	or		r8,		%0,			r16
+	zjmp	%:alive
+
+end_of_imb:	
+
+	sub		r8,		r3,			r8 #decrementation of forks number
+	fork	%:init_imb
+
 loop_init:
-	st r3, r4
-	ld %1, r15
-	st r1, 6
+
+	st		r3,		r4
+	ld		%1,		r15
+	st		r1,		6
+
+#each process has to wait NB process turn to be able to fork
+
 loop:
-	live %42
-	sub r4, r15, r4
-	zjmp %:alive
-	and %0, %0, r12
-	zjmp %:loop
+
+	live	%42
+	sub		r4,		r15,		r4
+	zjmp	%:alive
+	and		%0,		%0,			r16
+	zjmp	%:loop
+
+end_of_champ:
+
+endless:
+	and 	%0,		%0,			r16
+	zjmp	%:endless
+
 end:
