3ae63bf5aebba6ae08df2c28c9cde426937fdc1c
Cizeur <cgiron@e3r1p6.42.fr>
2019-09-27 19:08:10 +0200

    added message in case of malloc failure

--- arena/src/init.c
+++ arena/src/init.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 12:11:04 by cgiron            #+#    #+#             */
-/*   Updated: 2019/09/12 17:41:25 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/09/27 17:35:51 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,20 @@ void		init(t_master **mstr, t_opt *options)
 	int			i;
 
 	if (!(mstr_val = (t_master *)ft_memalloc(sizeof(t_master))))
+	{
+		ft_putstr("Malloc FAILURE\n");
 		exit_program(mstr_val);
+	}
 	i = -1;
 	ft_memcpy(&(mstr_val->options), options, sizeof(t_opt));
 	while (++i < MAX_PLAYERS)
 	{
 		if (!(mstr_val->players[i] =
 			(t_player *)ft_memalloc(sizeof(t_player) * MAX_PLAYERS)))
+		{
+			ft_putstr("Malloc FAILURE\n");
 			exit_program(mstr_val);
+		}
 	}
 	mstr_val->foamy_bat_cycle = CYCLE_TO_DIE;
 	mstr_val->nb_of_players = mstr_val->options.nb_players;
