9ca3d911b4bad0ceaa8d6f005363c67d2c8eefc8
Cizeur <cgiron@e1r5p8.42.fr>
2019-09-30 16:35:39 +0200

    assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -6,7 +6,7 @@
 #    By: advardon <advardon@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/19 17:57:30 by cgiron            #+#    #+#              #
-#    Updated: 2019/09/30 16:06:04 by cgiron           ###   ########.fr        #
+#    Updated: 2019/09/30 16:34:56 by cgiron           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,99 +21,104 @@ printf "Apply asm to .s file\n\
 	- wih program -p\n\
 	- with valgrind -v\n\
 	- with -vm for vm name\n\
-	- with -c for vm execution\n"
+	- with -c for vm execution\n\
+	- with -clean to remove all *.cor"
 }
 
-	########################## Variables initialization ############################
+########################## Variables initialization ############################
 
-	map_folder=.
-	program=./asm
-	corewar=0
-	valgrind=0
-	vm=./corewar
-	error=0
+map_folder=.
+program=./asm
+corewar=0
+valgrind=0
+vm=./corewar
+error=0
+clean=0
 
-	############################# Options parser ###################################
+############################# Options parser ###################################
 
-	while [ "$1" != "" ]; do
-		OPT=$1
-		case $OPT in
-			-h | --help)
-				usage
-				exit 0
-				;;
-			-v)
-				valgrind=1
-				;;
-	
-			-s)
-				error=1
-				;;
-			-p)
-				shift
-				program=$1
-				;;
-			-vm)
-				shift
-				vm=$1
-				;;
-			-c)
-				corewar=1
-				;;
-			-m)
-				shift
-				map_folder=$1
-				;;
-			*)
-				usage
-				exit
-				;;
-		esac
-		shift
-	done
+while [ "$1" != "" ]; do
+	OPT=$1
+	case $OPT in
+		-h | --help)
+			usage
+			exit 0
+			;;
+		-v)
+			valgrind=1
+			;;
+		-clean)
+			clean=1
+			;;
+		-s)
+			error=1
+			;;
+		-p)
+			shift
+			program=$1
+			;;
+		-vm)
+			shift
+			vm=$1
+			;;
+		-c)
+			corewar=1
+			;;
+		-m)
+			shift
+			map_folder=$1
+			;;
+		*)
+			usage
+			exit
+			;;
+	esac
+	shift
+done
 
-	################################ Main loop #####################################
+################################ Main loop #####################################
 
-	usage
 
-	echo "\n\nGENERATOR   :\033[0;32m" $generator_ex
-	echo "\033[0moptions :\033[0;32m" $options
-	echo "\033[0mMAP FOLDER :\033[0;32m" $map_folder
-	echo "\033[0mGENERATE TILL\033[0;32m" $n_maps "\033[0m MAPS"
-	echo "\033[0m"
+echo "\033[0mTEST FOLDER :\033[0;32m" $map_folder
+echo "\033[0m"
 
-		if test -f "$program"; then
-			echo "\033[0;32m$program exist\033[0m"
-		else
-			echo "\033[0;31m$program absent\033[0m"
-			exit 1
-		fi
+rm $map_folder/*.cor
 
+if [ "$clean" -eq 1 ]; then
+	exit 1
+fi
+
+if test -f "$program"; then
+	echo "\033[0;32m$program exist\033[0m"
+else
+	echo "\033[0;31m$program absent\033[0m"
+	exit 1
+fi
+if [ "$corewar" -eq 1 ]; then
+	if test -f "$vm"; then
+		echo "\033[0;32m$vm exist\033[0m"
+	else
+		echo "\033[0;31m$vm absent\033[0m"
+		exit 1
+	fi
+fi
+
+for filename in $map_folder/*.s; do
+	map_str=$filename
+	echo "Now testing \033[0;32m$map_str\033[0m"
+	$program $map_str
+	success=$($program $map_str | grep "Success")
+	if [ -z "$success" ]; then
+		cat -n $map_str
+	else
 		if [ "$corewar" -eq 1 ]; then
-		if test -f "$vm"; then
-			echo "\033[0;32m$vm exist\033[0m"
-		else
-			echo "\033[0;31m$vm absent\033[0m"
-			exit 1
+			./$vm "${filename%.s}.cor"
 		fi
-		fi
-
-		for filename in $map_folder/*.s; do
-			map_str=$filename
-			echo "Now testing \033[0;32m$map_str\033[0m"
-			$program $map_str
-			success=$($program $map_str | grep "Success")
-			if [ -z "$success" ]; then
-				cat -n $map_str
-			else
-				if [ "$corewar" -eq 1 ]; then
-					./corewar "${filename%.s}.cor"
-				fi
-			fi
-			if [ "$valgrind" -eq 1 ]; then
-				echo "With valgrind"
-				valgrind $program $map_str  >&1 
-			fi
-		done
+	fi
+	if [ "$valgrind" -eq 1 ]; then
+		echo "With valgrind"
+		valgrind $program $map_str  >&1 
+	fi
+done
 
 
